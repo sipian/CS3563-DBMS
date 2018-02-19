b67c5e8088ebc8848a2045d9ee99f8fe10eabba5
vishwakftw <cs15btech11043@iith.ac.in>
2018-02-19 18:40:55 +0530

    Simplify Person information extraction

--- populating-tables/person_information.py
+++ populating-tables/person_information.py
@@ -4,21 +4,17 @@ cur_db = pd.read_csv('name.basics.tsv', sep='\t',
                      usecols=['nconst', 'primaryName', 'birthYear', 'deathYear'],
                      low_memory=False)
 
-cur_db_to_records = cur_db.to_records()
+cur_db.rename(columns={
+                        'nconst': 'PersonID',
+                        'primaryName': 'PersonName',
+                        'birthYear': 'BirthYear',
+                        'deathYear': 'DeathYear'
+                      },
+              inplace=True)
 
-person_dict = {'PersonID': [], 'PersonName': [], 'BirthYear': [], 'DeathYear': []}
-
-ID = cur_db.columns.tolist().index('nconst') + 1
-NAME = cur_db.columns.tolist().index('primaryName') + 1
-BIRTH = cur_db.columns.tolist().index('birthYear') + 1
-DEATH = cur_db.columns.tolist().index('deathYear') + 1
-
-for rec in cur_db_to_records:
-    # Person processing
-    person_dict['PersonID'].append(rec[ID])
-    person_dict['PersonName'].append(rec[NAME])
-    person_dict['BirthYear'].append(rec[BIRTH])
-    person_dict['DeathYear'].append(rec[DEATH])
-
-persons = pd.DataFrame(person_dict, columns=sorted(person_dict.keys(), reverse=True))
-persons.to_csv('./title.person.csv', sep=',', na_rep="\\N", index=False, header=['PersonName', 'PersonID', 'DeathYear', 'BirthYear'])
+cur_db.to_csv('title.person.csv', sep=',', index=False)
+nid_list = cur_db['PersonID'].tolist()
+nid_file = open('PersonID_list.txt', 'w')
+for ID in nid_list:
+    nid_file.write('{}\n'.format(ID))
+nid_file.close()
