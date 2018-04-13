2cfa8f3f83ebfa86ce170efc756b9a546622b018
ysahil97 <cs15btech11044@iith.ac.in>
2018-04-13 22:07:14 +0530

    Added datasets, sql commands and initial pythonscript which sets up the database

---
+++ Kaggle Challenge/recommender.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+conn = psycopg2.connect("dbname=friendship1 user=postgres")
+
+cur = conn.cursor()
+
+#creating and filling in the tables
+cur.execute("CREATE TABLE Ratings (	UserID integer,	ProfileID integer,	Rating integer);")
+cur.execute("CREATE TABLE Gender (	UserID integer primary key,	Gender char(1));")
+
+# for now, ratings is filled as from the training data
+cur.execute("COPY Ratings FROM '/tmp/train_user_ratings.csv' CSV delimiter ',' NULL '\\N' ENCODING 'unicode' header;")
+cur.execute("COPY Gender FROM '/tmp/gender.csv' CSV delimiter ',' NULL '\\N' ENCODING 'unicode' header;")
+
+conn.commit()
+cur.close()
+conn.close()
+
+
+
