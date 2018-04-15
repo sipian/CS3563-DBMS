8d68179f348d16d6620dccb39374c80494da6f94
sipian <cs15btech11019@iith.ac.in>
2018-04-15 17:07:13 +0530

    threaded job handler

--- kaggle-challenge/recommender.py
+++ kaggle-challenge/recommender.py
@@ -5,8 +5,8 @@ conn = psycopg2.connect("dbname=friendship user=postgres password=postgres")
 cur = conn.cursor()
 
 # creating and filling in the tables
-cur.execute("CREATE TABLE Ratings (	UserID integer,	ProfileID integer,	Rating integer);")
-cur.execute("CREATE TABLE Gender (	UserID integer primary key,	Gender char(1));")
+cur.execute("CREATE TABLE ratings (	UserID integer,	ProfileID integer,	Rating integer);")
+cur.execute("CREATE TABLE gender (	UserID integer primary key,	Gender char(1));")
 
 # for now, ratings is filled as from the training data
 cur.execute("COPY Ratings FROM '/tmp/train_user_ratings.csv' CSV delimiter ',' NULL '\\N' ENCODING 'unicode' header;")
--- kaggle-challenge/user_user.py
+++ kaggle-challenge/user_user.py
@@ -1,10 +1,11 @@
 import psycopg2
 import numpy as np
 import pandas as pd
+import multiprocessing as mp
 
-conn = psycopg2.connect("dbname=friendship1 user=postgres password=Sahil23!" )
-cur = conn.cursor()
-
+no_of_process = 4
+test_data = pd.read_csv("./data/test_user_ratings.csv")
+unique_items = np.array(list(set(test_data["ForUserId"])))
 
 def _get_individual_info(userid, cur, total=220969):
     """
@@ -16,7 +17,7 @@ def _get_individual_info(userid, cur, total=220969):
     # idxs = req['ForUserId'].tolist()
     # rats = req['Rating'].tolist()
 
-    cur.execute("SELECT * FROM Train_Ratings WHERE userid = "+str(userid)+";")
+    cur.execute("SELECT ProfileID, Rating FROM ratings WHERE userid = "+str(userid)+";")
     rows = cur.fetchall()
     rows = np.array([list(i) for i in rows])
     idxs = rows[:,1]
@@ -27,13 +28,40 @@ def _get_individual_info(userid, cur, total=220969):
     rhe[idxs] = rats
     return rhe
 
-def get_batch_info(userids, total=220969):
+def get_batch_info(userids, cur, total=220969):
     """
     Gets a matrix of rating-hot-vectors for a given list of user ids from a pool of items
     """
     batch_matrix = np.zeros((len(userids), total))
     for i, uid in enumerate(userids):
-        batch_matrix[i] = _get_individual_info(uid, total)
+        batch_matrix[i] = _get_individual_info(uid, cur, total)
     return batch_matrix
 
-print(_get_individual_info(23408,cur))
+def jobHandler(i, total=220969):
+    global noOfProcess
+    conn = psycopg2.connect("dbname=friendship user=postgres password=postgres")
+    curr = conn.cursor()
+    batch_size = total/noOfProcess
+
+    start_index = (int)(i*batch_size + 1)
+
+    if i == noOfProcess-1:
+        end_index = total + 1
+    else:
+        end_index = (i+1)*batch_size + 1
+    end_index = (int)(end_index)
+
+    print("Process {} : {} - {}".format(i, start_index, end_index))
+    userids = np.arange(start_index, end_index)
+    return get_batch_info(userids , curr)
+
+
+pool = mp.Pool(processes=noOfProcess)
+results = [pool.apply(handleProcess, args=(i,)) for i in range(0,noOfProcess)]
+
+output = [p.get() for p in results]
+print(output.size())
+
+# normalized_mean queries
+# cur.execute("select userid, AVG(rating) into user_average_ratings from final_ratings group by userid;")
+# cur.execute("select final_ratings.userid, final_ratings.profileid, final_ratings.rating-user_average_ratings.avg from final_ratings INNER JOIN user_average_ratings on final_ratings.userid = user_average_ratings.userid;")
