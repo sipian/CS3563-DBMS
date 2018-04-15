95b9a8c1910190f98fb5cbbd321858b7fe417b84
vishwakftw <cs15btech11043@iith.ac.in>
2018-04-15 17:03:15 +0530

    Add batch prediction utilities

--- kaggle-challenge/user_user.py
+++ kaggle-challenge/user_user.py
@@ -1,6 +1,7 @@
 import psycopg2
 import numpy as np
 import pandas as pd
+import scipy.spatial as ssp
 
 conn = psycopg2.connect("dbname=friendship1 user=postgres password=Sahil23!" )
 cur = conn.cursor()
@@ -37,3 +38,47 @@ def get_batch_info(userids, total=220969):
     return batch_matrix
 
 print(_get_individual_info(23408,cur))
+
+def similarity_batch_individual(batch_userids, given_userid, cur_db, dist='cosine', total=220969):
+    """
+    Computes a similarity between items chosen a batch of users and a user
+    """
+    mat = get_batch_info(batch_userids, cur_db, dist, total)
+    vec = _get_individual_info(given_userid, cur_db, dist, total)
+    sims = ssp.distance.cdist(mat, vec.reshape(1, -1), dist)
+    return sims
+
+def similarity_batch_batch(batch_userids, batch_given_userids, cur_db, dist='cosine', total=220969):
+    """
+    Computes a similarity between items chosen by two batches of users
+    """
+    mat1 = get_batch_info(batch_userids, cur_db, dist, total)
+    mat2 = get_batch_info(batch_given_userids, cur_db, dist, total)
+    sims = ssp.distance.cdist(mat1, mat2, dist)
+    return sims
+
+def predict_individual(given_userid, given_itemid, cur_db, dist='cosine', total=220969):
+    """
+    Predicts the value for a user-item pair
+    """
+    req_users = cur_db.loc[cur_db['ForUserId'] == given_itemid, ['UserId', 'Rating']]
+    userlist = req_users['UserId'].tolist()
+    ratings = req_users['Rating'].tolist()
+    req_sim = similarity_batch_individual(userlist, given_userid, cur_db, dist, total).reshape(-1)
+    return np.dot(ratings, req_sim) / req_sim.sum()
+
+def predict_batch(given_userids, given_itemid, cur_db, dist='cosine', total=220969):
+    """
+    Predicts the value for a list of pairs [(u1, i), (u2, i), (u3, i), ..., (un, i)]
+    """
+    req_users = cur_db.loc[cur_db['ForUserId'] == given_itemid, ['UserId', 'Rating']]
+    userlist = req_users['UserId'].tolist()
+    ratings = np.array(req_users['Rating'].tolist())
+    req_sim = similarity_batch_batch(userlist, given_userids, cur_db, dist, total)
+    return ratings.dot(req_sim) / req_sim.sum(0)
+
+def get_users_for_item(given_itemid, cur_db):
+    """
+    Get a list of users paired with an item in the DB
+    """
+    return cur_db.loc[cur_db['ForUserId'] == given_itemid, 'UserId'].tolist()
