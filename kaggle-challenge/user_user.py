2fc37343b42cdcd2abe2d4b300ad5c22a1ca1452
vishwakftw <cs15btech11043@iith.ac.in>
2018-04-16 15:56:42 +0530

    Make modifications in the batches

--- kaggle-challenge/user_user.py
+++ kaggle-challenge/user_user.py
@@ -1,22 +1,18 @@
 import os
 import sys
 import psycopg2
-import threading
 import numpy as np
 import pandas as pd
 import scipy.spatial as ssp
 import multiprocessing as mp
 
-from threading import Thread
 from multiprocessing import Process
-
-no_of_thread = 4
 
 def _get_individual_info(userid, dist, total):
     """
     Gets a rating-hot-vector for a given user id from a pool of items
     """
-    req = tr_data.loc[tr_data['UserId'] == userid, ['ForUserId', 'Rating']]  # TODO: PostgreSQL
+    req = tr_data.loc[tr_data['UserId'] == userid, ['ForUserId', 'Rating']]
     idxs = req['ForUserId'].tolist()
     rats = req['Rating'].tolist()
 
@@ -59,7 +55,7 @@ def predict_individual(given_userid, given_itemid, dist='cosine', total=220971):
     """
     Predicts the value for a user-item pair
     """
-    req_users = tr_data.loc[tr_data['ForUserId'] == given_itemid, ['UserId', 'Rating']]  # TODO: PostgreSQL
+    req_users = tr_data.loc[tr_data['ForUserId'] == given_itemid, ['UserId', 'Rating']]
     userlist = req_users['UserId'].tolist()
     ratings = req_users['Rating'].tolist()
     req_sim = similarity_batch_individual(userlist, given_userid, dist, total).reshape(-1)
@@ -69,7 +65,7 @@ def predict_batch(given_userids, given_itemid, dist='cosine', total=220971):
     """
     Predicts the value for a list of pairs [(u1, i), (u2, i), (u3, i), ..., (un, i)]
     """
-    req_users = tr_data.loc[tr_data['ForUserId'] == given_itemid, ['UserId', 'Rating']]  # TODO: PostgreSQL
+    req_users = tr_data.loc[tr_data['ForUserId'] == given_itemid, ['UserId', 'Rating']]
     userlist = req_users['UserId'].tolist()
     ratings = np.array(req_users['Rating'].tolist())
     req_sim = similarity_batch_batch(userlist, given_userids, dist, total)
@@ -85,35 +81,23 @@ def get_users_for_item(given_itemid):
     """
     Get a list of users paired with an item in the DB
     """
-    return te_data.loc[te_data['ForUserId'] == given_itemid, 'UserId'].tolist()  # TODO: PostgreSQL
+    return te_data.loc[te_data['ForUserId'] == given_itemid, 'UserId'].tolist()
 
 
 def get_predictions_per_process(start, end):
     """
-    Get Predictions per thread 
+    Get Predictions per thread
     """
     print("Started Process {} ==> Range {} - {}".format(os.getpid(), start, end))
     unique_items = sorted_unique_items[start:end]
     for uitem in unique_items:
         te_users = get_users_for_item(uitem)
         batch_prediction = predict_batch(te_users, uitem)
-        pd.DataFrame(data=batch_prediction, columns=['UserId', 'ForUserId', 'Rating']).to_csv('./FILES/item={}.csv'.format(uitem), index=False, header=False)
+        batch_prediction[:, 2] = np.around(batch_prediction[:, 2], 6)
+        pd.DataFrame(data=batch_prediction,
+                     columns=['UserId', 'ForUserId', 'Rating']
+                     ).to_csv('./FILES/item={}.csv'.format(uitem), index=False, header=False)
     print("Completed Process {} ==> Range {} - {}".format(os.getpid(), start, end))
-
-# def spawn_threads(start, end):
-#     """
-#     Spawn no_of_thread threads and predict
-#     """
-#     batch_size = (end - start) / no_of_thread
-#     threads = []
-#     for x in range(no_of_thread):
-#         end_index = end if (start + batch_size > end) else start + batch_size
-#         threads.append(Thread(target=get_predictions_per_thread, args=((int)(start), (int)(end_index))))
-#         threads[-1].start()
-#         start += batch_size
-    
-#     for x in threads:
-#         x.join()
 
 def spawn_processes(batches):
     """
@@ -123,7 +107,7 @@ def spawn_processes(batches):
     for s, e in batches:
         process.append(Process(target=get_predictions_per_process, args=(s, e)))
         process[-1].start()
-    
+
     for p in process:
         p.join()
 
@@ -135,19 +119,25 @@ if __name__ == '__main__':
     sorted_unique_items = te_data.groupby(['ForUserId'])['UserId'].count().sort_values().index 
 
     print("Find value from 0 - 100000 in batch size of 2500")
-    batches = [(i, i+2500) for i in range(0,100001,2500)]
+    batches = [(2500 * i, 2500 * (i + 1) for i in range(0, 40)]
     spawn_processes(batches)
 
-    print("Find value from 120000 - 125000 in batch size of 2500")
-    batches = [(i, i+2500) for i in range(120000,125001,2500)]
+    print("Find value from 100000 - 125000 in batch size of 2500")
+    batches = [(2500 * i, 2500 * (i + 1)) for i in range(40, 65)]
     spawn_processes(batches)
 
     print("Find value from 125000 - 127000 in batch size of 500")
-    batches = [(i, i+500) for i in range(125000,127001,500)]
+    batches = [(125000 + i * 500, 125000 + (i + 1) * 500) for i in range(0, 4)]
     spawn_processes(batches)
 
-    print("Find value from 127000 - {} in batch size of 100".format(unique_items_count))
-    batches = [(i, i+100) for i in range(127000,unique_items_count,100)]
+    print("Find value from 127000 - 127200 in batch size of 100".format(unique_items_count))
+    batches = [(127000, 127100), (127100, 127200)]
     spawn_processes(batches)
-    
-    print("Done")                        
+
+    print("Find value from 127200 - 127252 in batch size of 26".format(unique_items_count))
+    batches = [(127200, 127226), (127226, 127252)]
+
+    print("Find value from 127252 - {} in batch size of 13".format(unique_items_count))
+    batches = [(127252 + i * 13, 127252 + (i + 1) * 13) for i in range(0, 4)]
+
+    print("Done")
