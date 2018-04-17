421b48f49279b0a27adf9fe5e644e113c407a25f
sipian <cs15btech11019@iith.ac.in>
2018-04-17 12:16:28 +0530

    submission

---
+++ kaggle-challenge/submission/README.md
@@ -0,0 +1,47 @@
+# Our idea : `relation_user_user.py`
+
+### Assumptions
+
++ Assume that the friendship is directed, which means that if (u, i) is in the friendship graph, then it is not necessary that (i, u) will be present.
+
++ Assume that the phrase "x is a friend of y" means "x" has given "y" a rating.
+
+### Algorithm
+
++ Given a pair (u, i), we find out all the users who are friends with "i".
++ Now obtain the ratings given by these users to "i" and the number of ratings given in all per user.
++ For every user who is a friend of "i", weigh the rating given to "i" proportionally to the number of ratings given by that user.
+    + This is in some-sense a feedback oriented system.
++ Perform the same thing, but with "u" instead of "i". Here we are interested in the friends of "u", rather than the people who have "u" as a friend.
++ Now take the weighted ratings, and weigh these ratings by the connections of each side - number of people who have "i" as a friend and number of friends of "u" respectively.
++ This will give the final rating.
+
+# Our idea : `hybrid_user_user.py`
+
+### Algorithm
+
++ This is the essentially the first part of the algorithm, where we don't consider the user side of the rating, both only the item side. (Step 1 to 3)
++ All terminology remains the same.
+
+
+
+# Other Attempts : `parallel-cosine-similarity-user-user.py`
+Apart from the two results we submitted above, we also tried other approaches.
+But these were not chosen due to increase in computation time.
+
+### Using Cosine Similarity :: 
+
+**Parallelism** : We make many processeses which loop over all the distinct ForUSerID in test-data (127304 iterations in tottal). To not overwhelm the memory, we perform a bunch of computation parallely and then after joining all the processes , start the next set of test-
+data points.
+
+**Batch computation** : To reduce computation time, we read all UserID to be rated for 1 ForUserID (i) in the test data. Then we get all ratings from train-data for all UserIDs who have actually rated for i. We then obtain a rating-hot vector for these 2 lists and find cosine-distance. This will give us the similarity metric between 2 UsersID. We do a dot product with the given ratings to obtain the final rating.
+
+**SubSampling** : When we read the list of UserIDs from train-data, we take the top 25 ones to make computation faster. 
+
+We did not submit this because this was taking ~6hrs to compute and validation accuracy was'nt that good.
+
+
+#### Points on how to run
+Since postgres can have problem of being run by postgres user [this](http://suite.opengeo.org/docs/latest/dataadmin/pgGettingStarted/firstconnect.html#allowing-local-connections) can be used to allow to run from normal user. 
+Also it is recommended to run the python program with sudo.
+If there is an error message of `train_user_ratings.csv` file not found, the relative path for all the csv can be changed to an absolute path in the python code.
---
+++ kaggle-challenge/submission/other attempts/parallel-cosine-similarity-user-user.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import psycopg2
+import numpy as np
+import pandas as pd
+import scipy.spatial as ssp
+import multiprocessing as mp
+from multiprocessing import Process
+
+def _get_individual_info(userid, dist, total):
+    """
+    Gets a rating-hot-vector for a given user id from a pool of items
+    """
+    req = tr_data.loc[tr_data['UserId'] == userid, ['ForUserId', 'Rating']]
+    idxs = req['ForUserId'].tolist()
+    rats = req['Rating'].tolist()
+
+    # rhe stands for "rating hot encoding"
+    if dist == 'cosine':
+        rhe = np.full((total,), -1)
+    else:
+        rhe = np.zeros((total,))
+    rhe[idxs] = rats
+    return rhe
+
+def get_batch_info(userids, dist='cosine', total=220971):
+    """
+    Gets a matrix of rating-hot-vectors for a given list of user ids from a pool of items
+    """
+    batch_matrix = np.zeros((len(userids), total))
+    for i, uid in enumerate(userids):
+        batch_matrix[i] = _get_individual_info(uid, dist, total)
+    return batch_matrix
+
+def similarity_batch_individual(batch_userids, given_userid, dist='cosine', total=220971):
+    """
+    Computes a similarity between items chosen a batch of users and a user
+    """
+    mat = get_batch_info(batch_userids, dist, total)
+    vec = _get_individual_info(given_userid, dist, total)
+    sims = ssp.distance.cdist(mat, vec.reshape(1, -1), dist)
+    return sims
+
+def similarity_batch_batch(batch_userids, batch_given_userids, dist='cosine', total=220971):
+    """
+    Computes a similarity between items chosen by two batches of users
+    """
+    mat1 = get_batch_info(batch_userids, dist, total)
+    mat2 = get_batch_info(batch_given_userids, dist, total)
+    sims = ssp.distance.cdist(mat1, mat2, dist)
+    return sims
+
+def predict_individual(given_userid, given_itemid, dist='cosine', total=220971):
+    """
+    Predicts the value for a user-item pair
+    """
+    req_users = tr_data.loc[tr_data['ForUserId'] == given_itemid, ['UserId', 'Rating']]
+    userlist = req_users['UserId'].tolist()
+    ratings = req_users['Rating'].tolist()
+    req_sim = similarity_batch_individual(userlist, given_userid, dist, total).reshape(-1)
+    return np.dot(ratings, req_sim) / req_sim.sum()
+
+def predict_batch(given_userids, given_itemid, dist='cosine', total=220971, max_rows=25):
+    """
+    Predicts the value for a list of pairs [(u1, i), (u2, i), (u3, i), ..., (un, i)]
+    """
+    req_users = tr_data.loc[tr_data['ForUserId'] == given_itemid, ['UserId', 'Rating']].head(max_rows)
+    userlist = req_users['UserId'].tolist()
+    ratings = np.array(req_users['Rating'].tolist())
+    req_sim = similarity_batch_batch(userlist, given_userids, dist, total)
+    denominator = req_sim.sum(0)
+    denominator[denominator == 0.0] = 1e-12
+    preds = ratings.dot(req_sim) / denominator
+    full_bunch = np.hstack([np.array(given_userids).reshape(-1, 1),
+                            np.full((len(given_userids), 1), given_itemid),
+                            preds.reshape(-1, 1)])
+    return full_bunch
+
+def get_users_for_item(given_itemid):
+    """
+    Get a list of users paired with an item in the DB
+    """
+    return te_data.loc[te_data['ForUserId'] == given_itemid, 'UserId'].tolist()
+
+
+def get_predictions_per_process(start, end):
+    """
+    Get Predictions per thread
+    """
+    print("Started Process {} ==> Range {} - {}".format(os.getpid(), start, end))
+    unique_items = sorted_unique_items[start:end]
+    for index, uitem in enumerate(unique_items):
+        te_users = get_users_for_item(uitem)
+        batch_prediction = predict_batch(te_users, uitem)
+        pd.DataFrame(data=batch_prediction, columns=['UserId', 'ForUserId', 'Rating']).to_csv('./FILES/item={}.csv'.format(uitem), index=False, header=False)
+        print("get_predictions_per_process :: ", )
+        batch_prediction[:, 2] = np.around(batch_prediction[:, 2], 6)
+        pd.DataFrame(data=batch_prediction,
+                     columns=['UserId', 'ForUserId', 'Rating']
+                     ).to_csv('./FILES/item={}.csv'.format(uitem), index=False, header=False)
+    print("Completed Process {} ==> Range {} - {}".format(os.getpid(), start, end))
+
+def spawn_processes(batches):
+    """
+    Spawn process to later spawn threads
+    """
+    process = []
+    for s, e in batches:
+        process.append(Process(target=get_predictions_per_process, args=(s, e)))
+        process[-1].start()
+
+    for p in process:
+        p.join()
+
+if __name__ == '__main__':
+
+    tr_data = pd.read_csv('train_user_ratings.csv', low_memory=False)
+    te_data = pd.read_csv('test_user_ratings.csv', low_memory=False)
+    unique_items_count = len(list(set(te_data["ForUserId"])))
+    sorted_unique_items = te_data.groupby(['ForUserId'])['UserId'].count().sort_values().index 
+
+    print("Find value from 0 - 100000 in batch size of 2500")
+    batches = [(2500 * i, 2500 * (i + 1) for i in range(0, 40))]
+    spawn_processes(batches)
+
+    print("Find value from 100000 - 125000 in batch size of 2500")
+    batches = [(2500 * i, 2500 * (i + 1)) for i in range(40, 65)]
+    spawn_processes(batches)
+
+    print("Find value from 125000 - 127000 in batch size of 500")
+    batches = [(125000 + i * 500, 125000 + (i + 1) * 500) for i in range(0, 4)]
+    spawn_processes(batches)
+
+    print("Find value from 127000 - 127200 in batch size of 100".format(unique_items_count))
+    batches = [(127000, 127100), (127100, 127200)]
+    spawn_processes(batches)
+
+    print("Find value from 127200 - 127252 in batch size of 26".format(unique_items_count))
+    batches = [(127200, 127226), (127226, 127252)]
+
+    print("Find value from 127252 - {} in batch size of 13".format(unique_items_count))
+    batches = [(127252 + i * 13, 127252 + (i + 1) * 13) for i in range(0, 4)]
+
+    print("Done")
